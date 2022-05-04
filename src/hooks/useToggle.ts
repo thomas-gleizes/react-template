496331b15298423cb27253f8bf660eb98740d7d1
Thomas <thomasgleizes34@gmail.com>
2022-05-04 09:41:18 +0200

    fix useToggle hook

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,8 +1,9 @@
-import { useState, useCallback } from "react";
+import { useState } from "react";
 
 const useToggle = (initialValues: boolean = false): [boolean, () => void] => {
   const [state, setState] = useState<boolean>(initialValues);
-  const toggle = useCallback((): void => setState(!state), []);
+
+  const toggle = (): void => setState(!state);
 
   return [state, toggle];
 };
